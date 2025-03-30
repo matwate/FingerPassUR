0a8009c9e1358c1431226ed7f195757ce6a76366
Andrés Sánchez <zeroryogi@gmail.com>
2025-03-30 18:55:05 -0500

    All the tools for FingerPassUR in a single repo. Also dockerized and created a docker-compose for the go api

--- README.md
+++
@@ -1,7 +0,0 @@
-# FingerPassUR Internal Server
-
-## Build
-Execute `go build ./cmd/api` to build the `api`
-
-## Run
-Execute `go run ./cmd/api` to run the `api`
--- internal/model/image.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type Image struct {
-	Id       int    `json:"id"       db:"id"`
-	Template string `json:"template" db:"path"`
-	User_id  int    `json:"user_id"  db:"user_id"`
-}
