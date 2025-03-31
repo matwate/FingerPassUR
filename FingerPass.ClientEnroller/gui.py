5304c0a1382aabc6bc1a2196f2a4f84a1eb86836
Andres Sanchez <zeroryogi@gmail.com>
2025-03-30 22:31:58 -0500

    Added all functionalyty

--- FingerPass.ClientEnroller/gui.py
+++ FingerPass.ClientEnroller/gui.py
@@ -1,7 +1,7 @@
-import requests
+import requests as r
 import subprocess
 import io
-
+import base64
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image as PILImage
@@ -13,10 +13,50 @@ image_buffers = [None, None, None, None, None]
 labels = []
 images_tk = [None, None, None, None, None]
 
+HOST="http://192.168.5.105:8080"
+
+routes = {
+    "create_user": "/user",
+    "create_image": "/image"
+}
+
+user_payload = {
+    "correo": None,
+    "nombre": None,
+    "programa": None,
+    "template": None
+}
+
+image_payload = {
+    "template": None,
+    "user_id": None
+}
+
 def submit():
     print(f"Name: {name_var.get()}")
     print(f"Email: {email_var.get()}")
     print(f"Program: {program_var.get()}")
+
+    user_payload["nombre"] = name_var.get()
+    user_payload["correo"] = email_var.get()
+    user_payload["programa"] = program_var.get()
+
+    image_buffers[0].seek(0)
+    user_payload["template"] = base64.b64encode(image_buffers[0].read()).decode("utf-8")
+    
+    print(image_buffers[0] == None)
+    print(user_payload["template"])
+
+    resp = r.post(f"{HOST}{routes["create_user"]}", json=user_payload)
+    user_id = resp.json()["user_id"]
+   
+    for i in range(1,5):
+        if image_buffers[i] is not None:
+            image_payload["user_id"] = user_id
+            image_buffers[i].seek(0)
+            image_payload["template"] = base64.b64encode(image_buffers[i].read()).decode("utf-8") 
+            resp = r.post(f"{HOST}{routes["create_image"]}", json=image_payload)
+            print(resp)
 
 
 def image_converter(filename: str):
@@ -30,7 +70,7 @@ def read_prints():
         ret_val = subprocess.call(["./urfp.1", "--create-image"])
         if ret_val == 0:
             buff = image_converter("img.pgm")
-            image_buffers.append(buff)
+            image_buffers[i] = buff
             image_tk = ImageTk.PhotoImage(PILImage.open(buff).resize((100,100)))
             images_tk[i] = image_tk
             labels[i].configure(image=image_tk)
